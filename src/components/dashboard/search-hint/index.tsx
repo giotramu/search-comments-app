c438b4fa7faa72616fcc5c44d6dbeabd613764a8
giotramu <14157578+giotramu@users.noreply.github.com>
2025-02-14 11:49:12 +0100

    feat: search hint details

--- src/components/dashboard/search-hint/index.tsx
+++ src/components/dashboard/search-hint/index.tsx
@@ -5,5 +5,6 @@ import type { Component } from 'solid-js'
 export const SearchHint: Component = () => (
   <>
     <strong>💡 Tip:</strong> Enter a minimum of {MIN_SEARCH_LENGTH} characters.
+    e.g. "Omnis dolorem".
   </>
 )
